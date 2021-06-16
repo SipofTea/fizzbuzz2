7f0c09e4943a6ad6c1e6cbd5da24bd771e329d46
SipofTea <61120785+SipofTea@users.noreply.github.com>
2021-06-16 13:41:06 +0000

    passed first test and written second test

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,5 +1,10 @@
+require 'fizzbuzz'
+
 describe 'fizzbuzz' do
-  it "return 'fizz' when given 3" do
-    expect(fizzbuzz(3)).to eq "fizz" 
+  it 'return "fizz" when given 3' do
+    expect(fizzbuzz(3)).to eq 'fizz' 
+  end
+  it 'return 4 when given 4' do
+    expect(fizzbuzz(4)).to eq 4
   end
 end
