29a1593cdf9f2621a6adb81dd4e4881d9d3bffb5
Brett Karlson <brettjkarlson@gmail.com>
2021-06-16 14:48:31 +0100

    wrote another test, and passed previous tests

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -7,4 +7,7 @@ describe 'fizzbuzz' do
   it 'return 4 when given 4' do
     expect(fizzbuzz(4)).to eq 4
   end
+  it 'returns "buzz" when given 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
 end
